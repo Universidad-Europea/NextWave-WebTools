116c9736b6e8c4ac702f822f9ed4c8a28f2c480c
DanielFernandez <dfernandezlouro1@gmail.com>
2022-05-09 13:08:42 +0200

    reloj funcional

--- res/js/watch.js
+++ res/js/watch.js
@@ -1,40 +1,30 @@
-setInterval(cargarReloj, 1000);
-function cargarReloj(){
- 
-    var systemDate = new Date();
-    var dd = systemDate.getDate();
-    var mm = systemDate.getMonth()+1;
-    var yyyy = systemDate.getFullYear();
-    var hh = systemDate.getHours();
-    var min = systemDate.getMinutes();
-    var ss = systemDate.getSeconds();
-    var horario = "AM";
-  
-    if(hh == 0){
-        hh = 12;
+
+
+// Function to convert time to string value
+const getTimeString = ({ hours, minutes, seconds, zone }) => {
+    if (minutes / 10 < 1) {
+      minutes = "0" + minutes;
     }
-
-    if(hh > 12){
-        hh = hh - 12;
-        horario = "PM";
+    if (seconds / 10 < 1) {
+      seconds = "0" + seconds;
     }
+    return `${hours}:${minutes}:${seconds} ${zone}`;
+  };
   
-    hh = (hh < 10) ? "0" + hh : hh;
-    min = (min < 10) ? "0" + min : min;
-    ss = (ss < 10) ? "0" + ss : ss;
+  // Function to display current time on screen
+  const renderTime = () => {
+    var currentTime = document.getElementById("current-time");
+    const currentDate = new Date();
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+    var zone = hours >= 12 ? "PM" : "AM";
+    if (hours > 12) {
+      hours = hours % 12;
+    }
+    const timeString = getTimeString({ hours, minutes, seconds, zone });
+    currentTime.innerHTML = timeString;
+  };
 
-    var time  = hh + ":" + min + ":" + ss + " " + horario;
-    
-    if(dd<10) {
-        dd = '0'+ dd;
-    } 
-    if(mm<10) {
-        mm = '0'+ mm;
-    } 
-    
-    systemDate = dd + '-' + mm + '-' + yyyy + "\n" + time;
-    document.getElementById("date").innerHTML = systemDate;
-}
- 
-
-cargarReloj();
+window.onload = renderTime;
+setInterval(renderTime, 1000);
