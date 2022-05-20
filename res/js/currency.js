d53dd9352b76d13121b1afcc0a2b73bd8b569d44
deveshaha <devesh_hanumante@outlook.com>
2022-05-20 09:42:05 +0200

    change src

---
+++ res/js/currency.js
@@ -0,0 +1,8 @@
+const dropList = document.querySelectorAll(".drop-list select");
+
+for (let i = 0; i < dropList.length; i++) {
+    for(currency_code in country_code){
+        console.log(country_code);
+    }
+    
+}
