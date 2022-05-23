addb729846ba011bdf69868124b033ffdfcdcb8c
Alex Gheorghe <alex@digitalsynware.com>
2022-05-23 12:13:28 +0200

    Fix qr generator, and minimal style

--- res/js/qr.js
+++ res/js/qr.js
@@ -6,13 +6,13 @@ qrImg = wrapper.querySelector(".qr-code img");
 generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value;
     if(!qrValue) return;
-    generateBtn.innerText = "Generating QR Code...";
+    generateBtn.innerText = "Generando...";
 
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
 
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
-        generateBtn.innerText = "Generate QR Code";
+        generateBtn.innerText = "Generador QR";
     })
  
 });
